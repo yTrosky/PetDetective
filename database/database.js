4e5aa7d53995b2f220fa2f3d6c5bca603127573e
Thiago Cruz Machado <96290053+yTrosky@users.noreply.github.com>
2024-05-27 15:06:37 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME
 });
