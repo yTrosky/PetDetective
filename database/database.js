48e7afc9fd58e6106e719bfc001b9f5480519c6b
Thiago Cruz Machado <96290053+yTrosky@users.noreply.github.com>
2024-06-25 20:21:54 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,13 +1,8 @@
 const Sequelize = require("sequelize");
 
-const mysql = require('mysql');
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME
+const connection = new Sequelize('guiapress','root','',{
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
